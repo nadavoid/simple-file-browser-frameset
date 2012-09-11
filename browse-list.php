7839d6eef624a4123c545ba9ccbaaeb34329af2d
David Lanier <dlanier@allplayers.com>
2012-09-11 17:50:47 -0600

    Change to using scandir instead of opendir, to easily sort items alphabetically.

--- browse-list.php
+++ browse-list.php
@@ -3,11 +3,11 @@ $dirs = scandir('./');
 print('<ul>');
 foreach($dirs as $dir) {
   if (is_dir($dir) && $dir != '.' && $dir != '..') {
-    if ($handle2 = opendir($dir)) {
+    if ($files = scandir($dir)) {
       print('<li>' . $dir . '<ul>');
-      while (false !== ($entry2 = readdir($handle2))) {
-        if (strpos($entry2, '.png')) {
-          print '<li><a href="./' . $dir . '/' . $entry2 . '" target="content">' . $entry2. '</a></li>' . "\n";
+      foreach($files as $file) {
+        if (strpos($file, '.png')) {
+          print '<li><a href="./' . $dir . '/' . $file . '" target="content">' . $file. '</a></li>' . "\n";
         }
       }
       print('</ul></li>');
